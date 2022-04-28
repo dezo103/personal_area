499dfb6ee6f70d79ee029fe18fa8ae84ef6f5ed2
Aliaksandr Rudyk <dezo103@gmail.com>
2022-04-28 16:52:47 +0300

    add find field form

--- src/components/ContactsArea/AddContactForm/AddContactForm.tsx
+++ src/components/ContactsArea/AddContactForm/AddContactForm.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import Card from '@mui/material/Card';
 
 import style from './AddContactForm.module.scss';
 
 export const AddContactForm = () => {
     return (
-        <div className = {style.addContactFormContainer}>
-            <TextField />
+        <Card className = {style.addContactFormContainer}>
+            <TextField label="Find contacts"/>
+            <TextField label="Add contacts"/>
             <Button variant='contained'
                     color='success'
-                    size='large'>Add Contact</Button>
-        </div>
+                    size='large'>Add</Button>
+        </Card>
     );
 };
--- src/components/ContactsArea/ContactsList/Contact/Contact.tsx
+++ src/components/ContactsArea/ContactsList/Contact/Contact.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Delete } from '@mui/icons-material';
+import IconButton from '@mui/material/IconButton';
 
 import {ContactPropsType} from '../../../../types/PropsTypes';
-//import style from './Contact/Contact.module.scss'
+import Card from "@mui/material/Card";
+import style from './Contact.module.scss';
 
 export const Contact = (props: ContactPropsType) => {
     return (
-        <div>
-            <li>{props.contactName}</li>
-        </div>
+        <Card style={{margin: '8px'}}>
+            <li className={style.contactItem}>
+                <span>{props.contactName}</span>
+                <IconButton>
+                    <Delete/>
+                </IconButton>
+            </li>
+        </Card>
     );
 };
 
